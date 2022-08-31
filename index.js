7e95a92503ed0565047cce29daa9811191a4e130
furxx2000 <furxx2000@gmail.com>
2022-08-31 23:05:25 +0800

    Compiled js file

--- index.js
+++ index.js
@@ -1,47 +1,28 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
+};
 const dice = document.querySelector(".dice");
 const adviceId = document.querySelector(".advice-id");
 const adviceContent = document.querySelector(".advice-content");
-const changeAdvice = () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const divider = document.querySelector(".divider img");
+const changeAdvice = () => __awaiter(void 0, void 0, void 0, function* () {
     const url = "https://api.adviceslip.com/advice";
     const res = yield fetch(url);
     const data = yield res.json();
     const { id, advice } = data.slip;
     adviceId.textContent = id;
     adviceContent.textContent = advice;
-  });
+});
 dice.addEventListener("click", changeAdvice);
+window.addEventListener("load", () => {
+    const isDesktop = matchMedia("(min-width: 1280px)").matches;
+    if (isDesktop)
+        divider.src = "./images/pattern-divider-desktop.svg";
+});
